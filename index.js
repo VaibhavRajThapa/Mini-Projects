8f6f8bfd7fc3466afa96e5160c3cf7d4213c26aa
Vaibhav <vaibhavrajthapawork@gmail.com>
2025-01-24 19:03:30 +0530

    Fixed the problem of color changing only once

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const body = document.querySelector("body")
 
 colors.forEach((color)=>{
     color.addEventListener("click", (event)=>{
-        if(event.id === "sky"){
+        if(color.id === "sky"){
             body.style.backgroundColor = "#0ea5e9";
-        }else if(event.id === "fuchsia"){
+        }else if(color.id === "fuchsia"){
             body.style.backgroundColor = "#c026d3";
-        }else if(event.id === "slate"){
+        }else if(color.id === "slate"){
             body.style.backgroundColor = "#475569";
         }else{
             body.style.backgroundColor = "#f43f5e";
